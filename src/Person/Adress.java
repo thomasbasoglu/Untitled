75ab07d0ffa715733afc3dabd91b055440678770
user.email <User@NESRIN>
2021-09-24 14:38:08 +0300

    first commit

---
+++ src/Person/Adress.java
@@ -0,0 +1,15 @@
+package Person;
+
+public class Adress {
+	
+	private String street;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String Country;
+	
+	public Adress() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Person/Person.java
@@ -0,0 +1,37 @@
+package Person;
+
+public class Person {
+	
+	private String Name;
+	private String phoneNumber;
+	private String emailAdress;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAdress() {
+		return emailAdress;
+	}
+
+	public void setEmailAdress(String emailAdress) {
+		this.emailAdress = emailAdress;
+	}
+
+}
